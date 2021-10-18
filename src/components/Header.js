71f4adae7bca94f48c760477dec15f46291c07d0
unknown <haquemizu@gmail.com>
2021-10-18 17:48:21 +0600

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Button from "./Button";
+
+const Header = ({ title, onAdd, showAdd }) => {
+  // const onClick =({title}) => {
+  //     console.log('click')
+  // }
+  return (
+    <div className="header">
+      <h1>{title}</h1>
+      {/* <button className='btn'>Add</button> */}
+      {/* <Button color='green' text='Add'  onClick={onAdd}  /> */}
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
+    </div>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracers",
+};
+
+export default Header;
